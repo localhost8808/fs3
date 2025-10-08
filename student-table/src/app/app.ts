34f0abaab2a8224664f7a03d831e68d88825e568
harshad-dhokane <worrk.harsshad@gmail.com>
2025-10-08 22:49:49 +0530

    updated html

--- student-table/src/app/app.ts
+++ student-table/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   imports: [CommonModule],
